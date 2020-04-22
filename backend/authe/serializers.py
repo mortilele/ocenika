228dbe548c16a5d79023b5aff7549516514aa6a7
Alik <@Alik>
2020-04-22 21:19:00 +0600

    authentication done

--- backend/authe/serializers.py
+++ backend/authe/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from authe.models import User
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'password']
